76c39bebb186d4800aeb2e43f4063970fc64694e
Rutujaasabe <97525707+Rutujaasabe@users.noreply.github.com>
2022-11-20 20:19:57 +0530

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,19 +1,17 @@
-	import java.util.*;
- 
-	class Day5
-	{
-                         public static void main(String arg[])
-	{
+  import java.util.*;
+  class Day5
+     {
+            public static void main(String arg[])
+	  {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Take a number from user : ");
 		int num = sc.nextInt();
-		if(num%2==0)
-		{
+		  if(num%2==0)
+		  {
 		    System.out.println("Number is even");
 			
-                                        }
+                        }
 		else
 		     System.out.println("Number is odd");
-		
-	}
+		}
 	}
